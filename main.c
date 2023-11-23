fd949cdcaed22777be248fd0d65503799a31e3ca
hickatheworld <cc@hicka.world>
2023-11-23 10:06:57 +0100

    Add search algorithms comparison and plotting script

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 #include "headers/cell.h"
 #include "headers/list.h"
+#include "headers/timer.h"
 
 /**
  * Demonstration of part 1 functions
@@ -31,11 +35,35 @@ void part1_demo() {
 void part2_search_demo() {
 	t_list *lst = create_sample_list(3);
 	display_list_aligned(lst);
-	printf("Is 1 in level 1 ? - %s\n", search_list_level(lst, 1, 1) ? "Yes" : "No");
-	printf("Is 1 in the list ? - %s\n", search_list(lst, 1) ? "Yes" : "No");
+	printf("Is 1 in level 1 ? - %s\n", search_list_level(lst, 1, 4) ? "Yes" : "No");
+	printf("Is 1 in the list ? - %s\n", search_list(lst, 4) ? "Yes" : "No");
 }
 
+void part2_comparison() {
+	t_list* sample;
+	int i, j, k, max;
+	for (i = 5; i <= 14; i++) {
+		max = pow(2, i) - 1;
+		printf("=== For n = %d ===\n", i);
+		printf("Level 0 search | Multi-level search\n");
+		sample = create_sample_list(i);
+		for (j = 1000; j <= 100000; j*= 10) {
+			printf("%d searches: ", j);
+			startTimer();
+			for (k = 0; k < j; k++)
+				search_list_level(sample, 0, (int)rand() % max + 1);
+			stopTimer();
+			printf("%s | ", getTimeAsString());
+			startTimer();
+			for (k = 0; k < j; k++)
+				search_list(sample, (int)rand() % max + 1);
+			stopTimer();
+			printf("%s\n", getTimeAsString());
+		}
+	}
+}
 
 int main() {
-	part2_search_demo();
+	srand(time(NULL));
+	part2_comparison();
 }
